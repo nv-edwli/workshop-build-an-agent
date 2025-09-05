4a7960857382eb83af84560c9e25fa2b9dee513a
nv-edwli <144396897+nv-edwli@users.noreply.github.com>
2025-09-05 14:02:07 -0500

    Update brev.nvwb-startup.sh

--- .project/brev.nvwb-startup.sh
+++ .project/brev.nvwb-startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 sudo -i -u ubuntu bash << 'EOF'
+readonly GIT_REPO=https://github.com/nv-edwli/workshop-build-an-agent
+readonly TARGET_BRANCH=develop
+readonly TARGET_APPLICATION=DevX-Lab
+
 # Download NVIDIA AI Workbench
 mkdir -p $HOME/.nvwb/bin
 curl -L https://workbench.download.nvidia.com/stable/workbench-cli/$(curl -L -s https://workbench.download.nvidia.com/stable/workbench-cli/LATEST)/nvwb-cli-$(uname)-$(uname -m) --output $HOME/.nvwb/bin/nvwb-cli
@@ -11,7 +15,10 @@ ln -sf $HOME/.nvwb/bin/nvwb-cli $HOME/.nvwb/bin/nvwb
 sudo $HOME/.nvwb/bin/nvwb-cli install --noninteractive --accept --docker --drivers --uid 1000 --gid 1000
 export PATH="$HOME/.nvwb/bin:$PATH"
 
-# Clone and build workshop
+# Clone workshop, avoid default branch build
 nvwb activate local
-nvwb clone project https://github.com/nv-edwli/workshop-build-an-agent --context local
+nvwb clone project $GIT_REPO --context local
+nvwb switch-branch $TARGET_BRANCH --context local --project $HOME/nvidia-workbench/*-build-an-agent
+nvwb build --context local --project $HOME/nvidia-workbench/*-build-an-agent
+nvwb start $TARGET_APPLICATION --context local --project $HOME/nvidia-workbench/*-build-an-agent
 EOF
